f45a0a8f5b4c3743934413bac8e4bea8e371d83b
Saurav Singh <sauravsingh69420@gmail.com>
2024-10-06 11:23:58 +0530

    Feat:made the navbar responsive with animations and added logo

--- mms/src/Home/Navbar/Navbar.jsx
+++ mms/src/Home/Navbar/Navbar.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Css/Navbar.css";
+import Logo from "../../Assets/img/favicon/favicon-32x32.png"
 const Navbar = () => {
+    const [isMenuOpen,setIsMenuOpen] = useState(false);
+    const handleToggle = ()=>{
+        setIsMenuOpen(!isMenuOpen);
+    }
     return (
         <nav className="navbar">
             <div className="navbar-logo">
-                <button className="link-button">Logo</button>
+                <button className="link-button"><img src={Logo}/></button>
             </div>
-            <ul className="navbar-menu">
+            <ul className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
                 <li><button className="link-button">Home</button></li>
                 <li><button className="link-button">Playlist</button></li>
                 <li><button className="link-button">Songs</button></li>
@@ -15,8 +20,8 @@ const Navbar = () => {
             <div className="navbar-button">
                 <button className="btn">Get Started</button>
             </div>
-            <div className="navbar-toggle">
-                <span className="toggle-icon">&#9776;</span>
+            <div className="navbar-toggle" onClick={handleToggle}>
+                <span className="toggle-icon">{isMenuOpen ? '\u00D7' : '\u2630'}</span>
             </div>
             
         </nav>

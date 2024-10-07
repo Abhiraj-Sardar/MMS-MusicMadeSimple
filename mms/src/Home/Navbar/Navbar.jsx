cb407a8824235efc8728763ec65c44e34ddbac46
Abhay Kumar Sahu <abhayksahu.cs@gmail.com>
2024-10-07 13:19:41 +0530

    added dark mode switch button and added all the dark mode functionalities in navbar component

--- mms/src/Home/Navbar/Navbar.jsx
+++ mms/src/Home/Navbar/Navbar.jsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import "./Css/Navbar.css";
 import Logo from "../../Assets/img/favicon/favicon-32x32.png"
+import Switch from '@mui/material/Switch';
 const Navbar = () => {
-    const [isMenuOpen,setIsMenuOpen] = useState(false);
-    const handleToggle = ()=>{
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [darkmode, setDarkmode] = useState(() => {
+        return localStorage.getItem("darkmode") === 'true';
+    });
+    const handleToggle = () => {
         setIsMenuOpen(!isMenuOpen);
+    }
+    useEffect(() => {
+        if (darkmode) {
+            document.body.classList.add('dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+        }
+        localStorage.setItem("darkmode", darkmode);
+
+    }, [darkmode])
+
+
+    const handleDarkmode = () => {
+        setDarkmode(!darkmode);
     }
     return (
         <nav className="navbar">
             <div className="navbar-logo">
-                <button className="link-button"><img src={Logo}/></button>
+                <button className="link-button"><img src={Logo} /></button>
             </div>
             <ul className={`navbar-menu ${isMenuOpen ? 'active' : ''}`}>
                 <li><button className="link-button">Home</button></li>
@@ -18,12 +36,16 @@ const Navbar = () => {
                 <li><button className="link-button">Coming Soon</button></li>
             </ul>
             <div className="navbar-button">
+                <div className='darkmode'>
+                    <Switch checked={darkmode} onChange={handleDarkmode} />
+                    <span >Dark Mode</span>
+                </div>
                 <button className="btn">Get Started</button>
             </div>
             <div className="navbar-toggle" onClick={handleToggle}>
                 <span className="toggle-icon">{isMenuOpen ? '\u00D7' : '\u2630'}</span>
             </div>
-            
+
         </nav>
     )
 }
